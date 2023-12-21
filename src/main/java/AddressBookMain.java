1775d3c9ed495d5235ddf5232f994179734e89eb
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-21 19:24:37 +0530

    Use Case 1 completed

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,7 +1,13 @@
-public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-        
+public class AddressBookMain {
+    static Map<String, Contact>AddressBook;
+    
+    public static void main(String[] args) {
+        AddressBook =new HashMap<>();
+        System.out.println("Welcome to Address Book Program");
     }
 }
