5addb52cab449979c3237832d3ee13bf9812f9b6
sanjeev <sanjeevmaths1729@gmail.com>
2023-12-21 19:38:35 +0530

    taken input and created new conatcts

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -5,9 +5,34 @@ import java.util.Scanner;
 
 public class AddressBookMain {
     static Map<String, Contact>AddressBook;
-    
+    public static void takeContactsInfo(){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your First Name");
+        String firstName=input.next();
+        System.out.println("Enter your Last Name");
+        String lastName=input.next();
+        System.out.println("Enter your Address");
+        String address=input.nextLine();
+        input.nextLine();
+        System.out.println("Enter your City");
+        String city=input.next();
+        System.out.println("Enter your State");
+        String state=input.next();
+        System.out.println("Enter your Zip");
+        int zip=input.nextInt();
+        System.out.println("Enter your Phone Number");
+        String phoneNumber=input.next();
+        System.out.println("Enter your Email");
+        String email=input.next();
+
+        System.out.println("Thank You for filling the details");
+
+        Contact newContact=new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
+        AddressBook.put(firstName,newContact);
+    }
     public static void main(String[] args) {
         AddressBook =new HashMap<>();
         System.out.println("Welcome to Address Book Program");
+        takeContactsInfo();
     }
 }
